e1ea8343402f7b8275afe27d06cb1fc89549fb74
LeilaAimetova15 <leilochkazai@gmail.com>
2024-09-21 18:53:44 +0300

    21\09

---
+++ main.py
@@ -0,0 +1,5 @@
+number_of_homework= 12
+number_of_hours = 1.5
+course_name = 'Pyton'
+time = number_of_hours / number_of_homework
+print ('Курс:', course_name,', всего задач:', number_of_homework, ', затрачено часов:', number_of_hours, ', среднее время выполнения:', time, 'часа.')
